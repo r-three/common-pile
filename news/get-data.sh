cefb4d2678844257ac9b1021e42897779ee06b7f
lintangsutawika <lintang@sutawika.com>
2024-01-09 02:53:47 +0000

    run individual news in process.py

--- news/get-data.sh
+++ news/get-data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-# for NEWS in democracy_no
-python news/process.py --url https://www.propublica.org/ --output_dir data/news-propublica/ --keywords article
+python news/process.py --url https://www.propublica.org/ --filename pro.jsonl.gz --output_dir data/news-propublica/ --keywords article
+python news/process.py --url https://www.democracynow.org/ --filename dem.jsonl.gz --output_dir data/news-democracy_now/ 
+
