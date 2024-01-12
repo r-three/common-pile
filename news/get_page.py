ae8baefa92f4774cf896a8cf761da7bb78995afe
lintangsutawika <lintang@sutawika.com>
2024-01-12 16:20:44 +0000

    remove duplicates in page_list

--- news/get_page.py
+++ news/get_page.py
@@ -53,6 +53,8 @@ def get_pages(page_index, output_path):
     url = page_index["url"]
     filename = page_index["filename"]
 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
     page_file_path = os.path.join(output_path, filename)
     # try:
     #     wget.download(url, out=page_file_path)
@@ -60,7 +62,14 @@ def get_pages(page_index, output_path):
     # except:
     # print("WGET Error", url)
     try:
-        page = requests.get(url)  
+        page = requests.get(
+            url,
+            headers=headers,
+            verify=False,
+            allow_redirects=False,
+            stream=True,
+            timeout=10
+        )
         with open(page_file_path, 'wb') as fp:
             fp.write(page.content)
         return (url, 0)
@@ -69,14 +78,14 @@ def get_pages(page_index, output_path):
 
 def main(args):
 
-    raw_output_dir = os.path.join(args.output_dir, "raw")
-    Path(raw_output_dir).mkdir(parents=True, exist_ok=True)
+    output_dir = args.output_dir
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     if args.index_path:
         with jsonlines.open(args.index_path) as reader:
             page_index = [line for line in reader]
     else:
-        pagelist_path = os.path.join(raw_output_dir, "pagelist.jsonl")
+        pagelist_path = os.path.join(output_dir, "pagelist.jsonl")
         if os.path.isfile(pagelist_path) and args.overwrite is False:
             with jsonlines.open(pagelist_path) as reader:
                 page_index = [line for line in reader]
@@ -86,23 +95,25 @@ def main(args):
             if args.limit is not None:
                 page_list = page_list[:args.limit]
 
+            # Remove duplicates
+            page_list = list(dict.fromkeys(page_list))
             page_index = [{"idx": idx, "url": url, "filename": f"{utils.sanitize_url(url)}.html"} for idx, url in enumerate(page_list)]
             with jsonlines.open(pagelist_path, mode="w") as writer:
                 writer.write_all(page_index)
     
     if args.dl:
         # Download all pages
-        download_fn = partial(get_pages, output_path=raw_output_dir)
+        download_fn = partial(get_pages, output_path=output_dir)
         num_workers = mp.cpu_count() if args.num_workers is None else args.num_workers
         if num_workers == 1:
             failed_pages = list(map(download_fn, tqdm(page_index)))
         else:
             with mp.Pool(num_workers) as p:
                 failed_pages = []
-                for page in tqdm(p.imap_unordered(download_fn, page_index), total=len(page_index)):
+                for page in tqdm(p.map(download_fn, page_index), total=len(page_index)):
                     failed_pages.append(page)
 
-        failedlist_path = os.path.join(raw_output_dir, "failedlist.jsonl")
+        failedlist_path = os.path.join(output_dir, "failedlist.jsonl")
         with jsonlines.open(failedlist_path, mode="w") as writer:
             writer.write_all([{"idx": idx, "url": url, "error": err} for idx, (url, err) in enumerate(failed_pages)])
 
