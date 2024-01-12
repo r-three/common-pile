3a191ad4f4c7249113d701848c08fd6e729ba9a2
lintangsutawika <lintang@sutawika.com>
2024-01-12 15:38:16 +0000

    add list of sites

--- news/get-data.sh
+++ news/get-data.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env sh
 
-python news/get_page.py --url https://www.propublica.org/ --output_dir data/news-propublica/
-python news/get_page.py --url https://www.democracynow.org/ --output_dir data/news-democracy_now/ 
-python news/get_page.py --url https://www.mongabay.com/ --output_dir data/news-mongabay/
-python news/get_page.py --url https://theconversation.com/ --output_dir data/news-theconversation/ 
+# CC-BY
+python news/get_page.py --url https://360info.org/ --output_dir data/news/360info/
+python news/get_page.py --url https://meduza.io/ --output_dir data/news/meduza/
+python news/get_page.py --url https://www.altnews.in/ --output_dir data/news/altnews/
+python news/get_page.py --url http://scidev.net/ --output_dir data/news/scidev/
+python news/get_page.py --url https://www.fides.org/en --output_dir data/news/fides/
+python news/get_page.py --url https://factly.in/ --output_dir data/news/factly/
+python news/get_page.py --url https://milwaukeenns.org/ --output_dir data/news/milwaukeenns/
+python news/get_page.py --url https://www.tasnimnews.com/en --output_dir data/news/tasnimnews/
+python news/get_page.py --url https://www.mekongeye.com/ --output_dir data/news/mekongeye/
+python news/get_page.py --url https://africasacountry.com/ --output_dir data/news/africasacountry/
+python news/get_page.py --url https://balkandiskurs.com/en/ --output_dir data/news/balkandiskurs/
+python news/get_page.py --url https://minorityafrica.org/ --output_dir data/news/minorityafrica/
+python news/get_page.py --url https://zimfact.org/ --output_dir data/news/zimfact/
+python news/get_page.py --url https://globalvoices.org/ --output_dir data/news/globalvoices/
+python news/get_page.py --url https://sojoexchange.solutionsjournalism.org/ --output_dir data/news/solutionsjournalism/
+python news/get_page.py --url https://freedom.press/ --output_dir data/news/freedom/
+python news/get_page.py --url https://www.newcanadianmedia.ca/ --output_dir data/news/newcanadianmedia/
+python news/get_page.py --url https://projectmultatuli.org/en/ --output_dir data/news/projectmultatuli/
 
+# CC BY-SA
+python news/get_page.py --url https://www.propastop.org/eng/ --output_dir data/news/propastop/
+python news/get_page.py --url https://www.thepublicrecord.ca/ --output_dir data/news/thepublicrecord/
+python news/get_page.py --url https://educeleb.com/ --output_dir data/news/educeleb/
+python news/get_page.py --url https://oxpeckers.org/ --output_dir data/news/oxpeckers/
+python news/get_page.py --url https://libertytvradio.com/ --output_dir data/news/libertytvradio/
+
+# Public Domain
+python news/get_page.py --url https://central.asia-news.com/en_GB/ --output_dir data/news/libertytvradio/
