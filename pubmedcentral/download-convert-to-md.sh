aa8f561356ea0c0b12e10520c9448f6f125fd2cd
Alon Albalak <alonalbalak@gmail.com>
2024-03-17 17:35:44 -0700

    wrap interpolations in quotes

--- pubmedcentral/download-convert-to-md.sh
+++ pubmedcentral/download-convert-to-md.sh
@@ -1,6 +1,6 @@
 #!/usr/bash/env sh
 
-TOTAL_DOCS=${1:-0}
+TOTAL_DOCS="${1:-0}"
 
 # downloads each file from the filelist and converts the .nxml file to .md
-python3 download_and_convert_to_md.py --filelist data/permissive_filelist.txt --total_docs $TOTAL_DOCS
+python3 download_and_convert_to_md.py --filelist data/permissive_filelist.txt --total_docs "${TOTAL_DOCS}"
--- pubmedcentral/get-filelist.sh
+++ pubmedcentral/get-filelist.sh
@@ -8,7 +8,7 @@ wget -nc https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_file_list.txt -P data/
 
 PERMISSIVE_FILELIST_DEST="data/permissive_filelist.txt"
 # -v invert match (find lines without these patterns), -w match whole word
-grep -vw "${NONPERMISSIVE_LICENSES}" data/oa_file_list.txt > ${PERMISSIVE_FILELIST_DEST}
+grep -vw "${NONPERMISSIVE_LICENSES}" data/oa_file_list.txt > "${PERMISSIVE_FILELIST_DEST}"
 
 # don't count the header line as a file
 echo "Found $(($(wc -l < ${PERMISSIVE_FILELIST_DEST})-1)) permissive licenses in data/oa_file_list.txt"
