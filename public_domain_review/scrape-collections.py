26f84b6146437afc2b8b9dec773cc8ec3cf8f9cf
Nikhil Kandpal <nkandpa2@gmail.com>
2024-05-01 06:41:02 -0400

    - factor out shared components into `utils.py` - add `run.sh`

--- public_domain_review/scrape-collections.py
+++ public_domain_review/scrape-collections.py
@@ -10,14 +10,12 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 
+from utils import SOURCE_NAME, get_outbound_links, get_content, contains_permissive_license, get_elements, get_elements_text
 from licensed_pile.licenses import PermissiveLicenses
 from licensed_pile.write import to_dolma
 
 
 logging.basicConfig(level=logging.INFO, format="scrape-collections: [%(asctime)s] [%(funcName)s] %(levelname)s - %(message)s")
-
-
-SOURCE_NAME = "public-domain-review"
 
 
 def parse_args():
@@ -36,22 +34,6 @@ def parse_args():
 
     args = parser.parse_args()
     return args
-
-
-def get_outbound_links(url, html):
-    soup = BeautifulSoup(html, "html.parser")
-    links = soup.find_all("a")
-    outbound_links = [urljoin(url, link.get('href')) for link in links if link.get('href')]
-    return set(outbound_links)
-
-
-def get_content(url):
-    logging.info(f"Downloaded {url}")
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    logging.error(f"{url} returned status {response.status_code}")
-    return None
 
 
 def generate_essay_links(args):
@@ -81,24 +63,6 @@ def generate_essay_links(args):
             seen_pages.add(page)
             page_links = [link for link in links if page_url_pattern.match(link) and link not in seen_pages]
             pages.update(page_links)
-
-
-def contains_permissive_license(html):
-    soup = BeautifulSoup(html, "html.parser")
-    license_statement = soup.find_all("div", class_="essay-license essay__content")[0].get_text()
-    return "CC BY-SA" in license_statement
-
-
-def get_elements(bs_obj, element_type, class_name):
-    return bs_obj.find_all(element_type, class_=class_name) 
-
-
-def get_elements_text(bs_obj, element_type, class_name):
-    elements = get_elements(bs_obj, element_type, class_name)
-    elements_text = [element.get_text().strip() for element in elements]
-    if len(elements_text) == 0:
-        elements_text = [""]
-    return elements_text
 
 
 def parse_essay_html(html):
@@ -133,7 +97,7 @@ def generate_records(args):
         essay_html = get_content(link)
         essay = parse_essay_html(essay_html)
         yield {
-            "id": link.strip("/").split("/")[-1]
+            "id": link.strip("/").split("/")[-1],
             "text": essay,
             "source": SOURCE_NAME,
             "type": "collection",
---
+++ public_domain_review/utils.py
@@ -0,0 +1,42 @@
+import logging
+import requests
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+
+
+SOURCE_NAME = "public-domain-review"
+
+
+def get_outbound_links(url, html):
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all("a")
+    outbound_links = [urljoin(url, link.get('href')) for link in links if link.get('href')]
+    return set(outbound_links)
+
+
+def get_content(url):
+    logging.info(f"Downloaded {url}")
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    logging.error(f"{url} returned status {response.status_code}")
+    return None
+
+
+def contains_permissive_license(html):
+    soup = BeautifulSoup(html, "html.parser")
+    license_statement = soup.find_all("div", class_="essay-license essay__content")[0].get_text()
+    return "CC BY-SA" in license_statement
+
+
+def get_elements(bs_obj, element_type, class_name):
+    return bs_obj.find_all(element_type, class_=class_name) 
+
+
+def get_elements_text(bs_obj, element_type, class_name):
+    elements = get_elements(bs_obj, element_type, class_name)
+    elements_text = [element.get_text().strip() for element in elements]
+    if len(elements_text) == 0:
+        elements_text = [""]
+    return elements_text
