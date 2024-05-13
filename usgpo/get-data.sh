78aced6767ee2590c8bb288eba88542845c6f361
Nikhil Kandpal <nkandpa2@gmail.com>
2024-05-13 10:36:13 -0400

    Driver script

---
+++ usgpo/get-data.sh
@@ -0,0 +1,14 @@
+set -e
+
+api_key=${1}
+start_date=${2}
+
+USGPO_DIRECTORY="data/usgpo"
+
+mkdir -p ${USGPO_DIRECTORY}/raw
+
+echo "Getting Document Links"
+python usgpo/get-links.py --api-key "${api_key}" --start-date "${start_date}" --output-dir ${USGPO_DIRECTORY}/raw
+
+echo "Downloading Documents"
+python usgpo/download-files.py --api-key ${api_key} --links-file ${USGPO_DIRECTORY}/raw/links.jsonl
