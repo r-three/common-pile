84ed2b20e2d317c691247b76ea9d40214e73d4ca
Brian Lester <blester125@users.noreply.github.com>
2024-05-28 15:56:29 -0400

    updates needed when processing the full dump (#79)

--- stackexchange/get-dumps.sh
+++ stackexchange/get-dumps.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
+data_dir=${1:-"data"}
+data_dir=${data_dir%/}
+
+echo "Downloading Sites map from Internet Archive and saving to ${data_dir}."
 # Get all the stackexchange dumps from archive.org
-wget https://archive.org/download/stackexchange/Sites.xml -P data/
+wget https://archive.org/download/stackexchange/Sites.xml -P ${data_dir}
 # List the download links based on the xml file and the archive.org base dir.
 # Shuffle so any restarts probably start with a new files
 # run 4 wget processins and call each on with 10 urls.
@@ -12,6 +16,6 @@ wget https://archive.org/download/stackexchange/Sites.xml -P data/
 # -P data/dump   save all files with the prefix data/dump
 # Note: The progress bars from the different wget processes overwrite each other
 # They are still useful to ensure that progress is being made.
-python list-sites.py | shuf | xargs -n10 -P4 wget -w 2 -t 10 -c -nc -P data/dump --show-progress
-./extract.sh
-./get-stackoverflow.sh
+python list-sites.py | shuf | xargs -n10 -P4 wget -w 2 -t 10 -c -nc -P ${data_dir}/dump --show-progress
+./extract.sh ${data_dir}
+./get-stackoverflow.sh ${data_dir}
