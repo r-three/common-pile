ecbc70dd51125f791a325d0683a94050224c2c38
Enrico Shippole <enricoship@gmail.com>
2024-06-03 13:53:51 -0400

    Lint for download_and_convert_md.py and add comments

--- courtlistener/get_data.sh
+++ courtlistener/get_data.sh
@@ -10,6 +10,9 @@ download_dir="./data/courtlistener/raw"
 # Create the directory if it does not exist
 mkdir -p "$download_dir"
 
+# Only download the data from most recent CL dump
+# The newest dump contains the previous dumps data 
+# Differences from the previous data should not be included
 dates=(
     "2024-05-06"
 )
