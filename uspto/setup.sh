a9f18f50fcfdaa9ba671826f7f1a5e209389d9de
baberabb <92168766+baberabb@users.noreply.github.com>
2024-06-11 17:35:48 +0500

    nit

--- uspto/setup.sh
+++ uspto/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Download HF Dataset repo
-git clone git@hf.co:datasets/baber/USPTO /uspto/data
-echo "HF Dataset repo cloned to /uspto/data"
+git clone git@hf.co:datasets/baber/USPTO uspto/data
+echo "HF Dataset repo cloned to uspto/data"
 
 # install pandoc
 #wget https://github.com/jgm/pandoc/releases/download/3.2/pandoc-3.2-1-amd64.deb
