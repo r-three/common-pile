c2a027f9ff3744941a5342cbf3d814c741346a12
Brian Lester <blester125@gmail.com>
2025-03-25 14:14:23 -0400

    fix lint

--- stackexchange/collect-ids.py
+++ stackexchange/collect-ids.py
@@ -3,31 +3,25 @@
 import argparse
 import glob
 import json
+
 import smart_open
 from tqdm import tqdm
+
 from licensed_pile import utils
 
-
 parser = argparse.ArgumentParser(description="Collect all ids from dolma files.")
+parser.add_argument("--input", required=True, help="The input dir.")
 parser.add_argument(
-    "--input",
-    required=True,
-    help="The input dir.")
-parser.add_argument(
-    "--output",
-    default="ids.json",
-    help="Where to save the resulting file."
+    "--output", default="ids.json", help="Where to save the resulting file."
 )
 parser.add_argument(
-    "--filename",
-    default="*.jsonl.gz",
-    help="The default file name glob pattern."
+    "--filename", default="*.jsonl.gz", help="The default file name glob pattern."
 )
 
 
 def main():
     args = parser.parse_args()
-    
+
     ids = set()
     for file_name in tqdm(glob.glob(utils.dolma_input(args.input, args.filename))):
         with smart_open.open(file_name) as f:
